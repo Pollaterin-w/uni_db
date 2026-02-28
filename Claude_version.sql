17bd33f7d5a78f96fccb7d96f1a39cb66a1c772b
Kateryna Polyuga <polyugakateryna@gmail.com>
2026-02-28 22:31:37 +0200

    fixed bugs, features get_users_role has_permission can_view_image

--- Claude_version.sql
+++ Claude_version.sql
@@ -71,11 +71,11 @@ CREATE TABLE IF NOT EXISTS shares (
     image_id INTEGER REFERENCES images(image_id) ON DELETE CASCADE, 
     folder_id INTEGER REFERENCES folder(folder_id) ON DELETE CASCADE, 
     share_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    update_share_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    update_share_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
 	CONSTRAINT check_share_target CHECK(
-		(image_id NOT NULL AND folder_id IS NULL) OR
-		(Image_id IS NULL AND folder_id NOT NULL)
+		(image_id IS NOT NULL AND folder_id IS NULL) OR
+		(image_id IS NULL AND folder_id IS NOT NULL)
 	),
 	  CONSTRAINT check_share_self CHECK (from_user_id <> to_user_id)
 	
@@ -84,4 +84,56 @@ CREATE TABLE IF NOT EXISTS shares (
 CREATE INDEX idx_shares_to_user ON shares(to_user_id);
 CREATE INDEX idx_shares_image ON shares(image_id);
 CREATE INDEX idx_shares_folder ON shares(folder_id);
+
+CREATE OR REPLACE FUNCTION get_users_role (p_user_id SERIAL)
+RETURN role_name_domain AS $$
+DECLARE
+	v_role role_name_domain;
+BEGIN 
+	SELECT role.role_name INTO v_role
+	FROM users
+	JOIN role 
+	ON users.role_id = role.role_id
+	WHERE users.user_id = p_user_id
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION has_permission(p_user_id INTEGER, p_permission_name VARCHAR(100))
+RETURNS BOOLEAN AS $$
+DECLARE
+    v_has BOOLEAN;
+BEGIN
+    SELECT EXISTS(
+        SELECT 1
+        FROM users
+        JOIN role_permission ON users.role_id = role_permission.role_id
+        JOIN permission ON role_permission.permission_id = permission.permission_id
+        WHERE users.user_id = p_user_id
+          AND permission.permission_name = p_permission_name
+    ) INTO v_has;
+
+    RETURN v_has;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION can_view_image(p_user_id INTEGER, p_image_id INTEGER)
+RETURN BOOLEAN AS $$
+DECLARE
+BEGIN
+	WHERE i.image_id = p_image_id
+		AND (
+        i.owner_id = p_user_id        
+        OR i.is_public = TRUE         
+     	    OR EXISTS (                  
+            SELECT 1 FROM shares
+            WHERE to_user_id = p_user_id
+                AND image_id = p_image_id
+      )
+  )
+END;
+$$ LANGUAGE plpgsql;
+
+
 
